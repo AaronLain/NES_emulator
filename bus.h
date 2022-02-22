72c86c86b875cca03a3c341f03fbba480e9911cb
Aaron Lain <aaron.j.lain@gmail.com>
2022-02-22 12:44:51 -0600

    setup + 6502 addressing modes

---
+++ bus.h
@@ -0,0 +1,22 @@
+
+#include <cstdint>
+#include "olc6502.h"
+#include <array>
+
+class Bus {
+public:
+    Bus();
+    virtual ~Bus();
+
+public:
+    olc6502 cpu;
+
+    std::array<uint8_t, 64 * 1024> ram;
+
+public:
+    void write(uint16_t addr, uint8_t data);
+    uint8_t read(uint16_t addr, bool bReadOnly = false);
+};
+
+
+
---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 21, 2022
+ *      Author: user
+ */
+
+#include <iostream>
+
+int main() {
+
+	return 0;
+}
+
+
+
