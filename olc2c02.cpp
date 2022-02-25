0d52769f3fc7213ef602f6b6dc23b8be00c5ba3f
Aaron Lain <aaron.j.lain@gmail.com>
2022-02-25 12:26:41 -0600

    ppu registers and infrastructure

--- olc2c02.cpp
+++ olc2c02.cpp
@@ -16,6 +16,9 @@ uint8_t olc2C02::cpuRead(uint16_t addr, bool rdonly)
     case 0x0001: // Mask
         break;
     case 0x0002: // Status
+        data = (status.reg & 0xE0) | (ppu_data_buffer & 0x1F);
+        status.vertical_blank = 0;
+        address_latch = 0;
         break;
     case 0x0003: // OAM Address
         break;
@@ -26,6 +29,11 @@ uint8_t olc2C02::cpuRead(uint16_t addr, bool rdonly)
     case 0x0006: // PPU Address
         break;
     case 0x0007: // PPU Data
+        data = ppu_data_buffer;
+        ppu_data_buffer = ppuRead(ppu_address);
+
+        if (ppu_address > 0x3f00) data = ppu_data_buffer;
+        ppu_address++;
         break;
     }
 
@@ -37,8 +45,10 @@ void olc2C02::cpuWrite(uint16_t addr, uint8_t data)
     switch (addr)
     {
     case 0x0000: // Control
+        control.reg = data;
         break;
     case 0x0001: // Mask
+        mask.reg = data;
         break;
     case 0x0002: // Status
         break;
@@ -49,8 +59,19 @@ void olc2C02::cpuWrite(uint16_t addr, uint8_t data)
     case 0x0005: // Scroll
         break;
     case 0x0006: // PPU Address
+        if (address_latch == 0)
+        {
+            ppu_address = (ppu_address & 0x00FF) | (data << 8);
+            address_latch = 1;
+        }
+        else
+        {
+            ppu_address = (ppu_address & 0xFF00) | data;
+            address_latch = 0;
+        }
         break;
     case 0x0007: // PPU Data
+        ppuWrite(ppu_address, data);
         break;
     }
 }
@@ -62,6 +83,20 @@ void olc2C02::ConnectCartridge(const std::shared_ptr<Cartridge>& cartridge)
 
 void olc2C02::clock()
 {
+    if (scanline == -1 && cycle == 0)
+    {
+        status.vertical_blank = 0;
+    }
+
+    if (scanline == 241 && cycle == 1)
+    {
+        status.vertical_blank = 1;
+        if (control.enable_nmi)
+        {
+            nmi = true;
+        }
+    }
+
     cycle++;
     if (cycle >= 341)
     {
