888148188813235b5b262129bf4f23740930c39f
Aaron Lain <aaron.j.lain@gmail.com>
2022-03-02 22:45:37 -0600

    sequencer

--- olc2a03.h
+++ olc2a03.h
@@ -19,6 +19,31 @@ public:
     double GetOutputSample();
 
 private:
+
+    struct sequencer
+    {
+        uint32_t sequence = 0x00000000;
+        uint16_t timer = 0x0000;
+        uint16_t reload = 0x0000;
+        uint8_t output = 0x00;
+
+        uint8_t clock(bool bEnable, std::function<void(uint32_t &s)> funcManip)
+        {
+            if(bEnable)
+            {
+                timer--;
+                if (timer == 0xFFFF)
+                {
+                    timer = reload + 1;
+                    funcManip(sequence);
+                    output = sequence & 0x00000001;
+                }
+            }
+
+            return output;
+        }
+    };
+
     bool pulse1_enable = false;
     double pulse1_sample = 0.0;
 };
